d994a1da518a80d45795e6c58284174a0bff1a46
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-12-14 12:37:18 +0100

    wersja prosta, z bledem, ale tak sie konczy rozdzial ;)

---
+++ src/ProstyPortalGra.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ProstyPortalGra {
+    public static void main(String[] args) {
+        int iloscRuchow = 0;
+        PomocnikGry pomocnik = new PomocnikGry();
+
+        ProstyPortal portal = new ProstyPortal();
+
+        int liczbaLosowa = (int)(Math.random() * 5);
+
+        int[] polozenie = {liczbaLosowa, liczbaLosowa + 1, liczbaLosowa + 2};
+        portal.setPolaPolozenia(polozenie);
+        boolean czyIstnieje = true;
+
+        while(czyIstnieje == true) {
+            String pole = pomocnik.pobierzDaneWejsciowe("Podaj liczbe: ");
+            String wynik = portal.sprawdz(pole);
+
+            iloscRuchow++;
+
+            if(wynik.equals("zatopiony")) {
+                czyIstnieje = false;
+                System.out.println(iloscRuchow + " ruchow");
+            }
+        }
+    }
+}
+
+class ProstyPortal {
+    int[] polaPolozenia;
+    int iloscTrafien;
+
+    public void setPolaPolozenia(int[] ppol) {
+        polaPolozenia = ppol;
+    }
+    public String sprawdz(String stringPole) {
+        int strzal = Integer.parseInt(stringPole);
+        String wynik = "pudlo";
+        for(int pole : polaPolozenia) {
+            if (strzal == pole) {
+                wynik = "trafiony";
+                iloscTrafien++;
+                break;
+            }
+        }
+        if (iloscTrafien == polaPolozenia.length) {
+            wynik = "zatopiony";
+        }
+        System.out.println(wynik);
+        return wynik;
+    }
+}
+
+class PomocnikGry {
+    public String pobierzDaneWejsciowe (String komunikat) {
+        String wierszWej = null;
+        System.out.println(komunikat + " ");
+        try {
+            BufferedReader sw = new BufferedReader(new InputStreamReader(System.in));
+            wierszWej = sw.readLine();
+            if (wierszWej.length() == 0) return null;
+        }
+        catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return wierszWej;
+    }
+}
